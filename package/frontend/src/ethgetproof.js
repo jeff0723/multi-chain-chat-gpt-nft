b90929c139195c889e3d9d147ea986ad1e247cd7
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2023-06-02 23:48:07 +0900

    ethgetproof

--- package/frontend/src/ethgetproof.js
+++ package/frontend/src/ethgetproof.js
@@ -4,10 +4,10 @@ dotenv.config();
 
 // after the client calls getBlockHeader, and you now have the block header
 // use this function to get the relevant account and storage proofs
-async function getProofs(blockhash) {
+// nft is a struct that containsthe tokenId and the imageURL
+async function getProofs(nft, blockhash) {
+  const signature = window.localStorage.getItem("signature");
   const RPC = "https://mainnet.infura.io/v3/dc7c60b22021400a97355601e710833d";
-  const snapshopBlock =
-    "0xab60720eb3fb4bba53e99959153dcdc44cd269b6a48a66d3aa7a6c5b5a906eb0";
   const provider = new ethers.providers.JsonRpcProvider(RPC);
 
   const blockNumber = await provider.send("eth_getBlockByHash", [
@@ -27,5 +27,12 @@ async function getProofs(blockhash) {
 
   const storageRoot = blockInfo["result"]["stateRoot"];
 
-  await claimAzuki(); // !!! put inputs here
+  await claimAzuki(
+    nft.tokenId,
+    nft.imageUrl,
+    signature,
+    proof.storageHash,
+    proof.accountProof,
+    proof.storageProof[0].proof
+  );
 }
--- package/frontend/utils/constants.js
+++ package/frontend/utils/constants.js
@@ -1,3 +1,8 @@
 import { ethers } from "ethers";
 
 export const AzukiAddress = "0xED5AF388653567Af2F388E6224dC7C4b3241C544";
+
+export const AzukiSlotOf = (tokenId) =>
+  ethers.utils.keccak256(
+    ethers.utils.solidityPack(["uint256", "uint256"], [tokenId, 103])
+  );
