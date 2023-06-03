9c62dbe494f2faf7d57d89c23741becbc855d107
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-06-03 12:26:13 +0900

    change slot

--- package/frontend/utils/constants.js
+++ package/frontend/utils/constants.js
@@ -1,8 +1,8 @@
 import { ethers } from "ethers";
 
 export const AzukiAddress = "0xED5AF388653567Af2F388E6224dC7C4b3241C544";
-
+export const VARIABLE_POSITION = 2;
 export const AzukiSlotOf = (tokenId) =>
   ethers.utils.keccak256(
-    ethers.utils.solidityPack(["uint256", "uint256"], [tokenId, 103])
+    ethers.utils.solidityPack(["uint256", "uint256"], [tokenId, VARIABLE_POSITION])
   );
