6ffc4f5643cc7d67f49a4aa7c31de12a088c8598
jeffreylin0723 <qwertzxc1478963@gmail.com>
2023-06-04 03:47:36 +0900

    add default system

--- package/frontend/src/helper/generateSystemMessage.ts
+++ package/frontend/src/helper/generateSystemMessage.ts
@@ -1,5 +1,6 @@
 export const generateSystemMessage = (
     { name, description, personality, example, prompt }: { name: string, description: string, personality: string, example: string, prompt: string }
 ) => {
+    if (!name || !description || !personality || !example || !prompt) return 'You are a regular chatgpt!'
     return `You are a helpful ${name} AI, your name is \"${name}\". You are: ${personality}. ${description}. Here is an example of how you might respond to a user: '${example}'`
 }
