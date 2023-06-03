df9f46b8789659b3e509310c2191b07aaed01584
taetaehoho <75167060+taetaehoho@users.noreply.github.com>
2023-06-03 17:23:19 +0900

    add test

---
+++ package/hardhat/test/constant.js
@@ -0,0 +1,127 @@
+export const HASHI_VERIFIER_ABI = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "hashiaddress",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "p",
+        type: "bytes32",
+      },
+    ],
+    name: "Debug",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "InvalidAccount",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "InvalidHashiHash",
+    type: "error",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "index",
+        type: "uint256",
+      },
+    ],
+    name: "InvalidProof",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "InvalidStateProof",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "InvalidStorageProof",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "getBlockHeader",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "hashiheader",
+    outputs: [
+      {
+        internalType: "bytes32",
+        name: "",
+        type: "bytes32",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes32",
+        name: "blockheader",
+        type: "bytes32",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "signature",
+        type: "bytes",
+      },
+      {
+        internalType: "bytes32",
+        name: "stateRoot",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes32",
+        name: "storageRoot",
+        type: "bytes32",
+      },
+      {
+        internalType: "bytes[]",
+        name: "stateProof",
+        type: "bytes[]",
+      },
+      {
+        internalType: "bytes[]",
+        name: "storageProof",
+        type: "bytes[]",
+      },
+    ],
+    name: "verifyOwner",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+export const HASHI_VERIFIER_ADDRESS =
+  "0x5FbDB2315678afecb367f032d93F642f64180aa3";
---
+++ package/hardhat/test/findVariablePositionHardhat.js
@@ -0,0 +1,92 @@
+import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+import dotenv from "dotenv";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+dotenv.config();
+
+const mainnetRPC =
+  "https://mainnet.infura.io/v3/dc7c60b22021400a97355601e710833d";
+const mainnetProvider = new ethers.providers.JsonRpcProvider(mainnetRPC);
+//@ts-ignore
+const mainnetSigner = new ethers.Wallet(process.env.PK, mainnetProvider);
+
+const hardhatSigner = await ethers.getSigners();
+console.log(hardhatSigner);
+
+const PudgySlot = (tokenId, variable_position) =>
+  ethers.utils.keccak256(
+    ethers.utils.solidityPack(
+      ["uint256", "uint256"],
+      [tokenId, variable_position]
+    )
+  );
+
+async function getSignature() {
+  const message = ethers.utils.defaultAbiCoder.encode(
+    ["address"],
+    ["0xdb4a057C4054E030e6F04619777c17CafA33cf65"]
+  );
+  const signature = await mainnetSigner?.signMessage(
+    ethers.utils.arrayify(message)
+  );
+  return signature;
+}
+
+async function testContract(tokenId) {
+  const signature = await getSignature();
+
+  const blockNumber = "0x1097bfb";
+
+  // await mainnetProvider.send("eth_blockNumber");
+  const blockHash = await mainnetProvider.send("eth_getBlockByNumber", [
+    blockNumber,
+    false,
+  ]);
+
+  const stateRoot = blockHash["stateRoot"];
+
+  for (let i = 0; i < 1; i++) {
+    const slot = PudgySlot(tokenId, i);
+    const proof = await mainnetProvider.send("eth_getProof", [
+      // hardcode pudgy address
+      "0xBd3531dA5CF5857e7CfAA92426877b022e612cf8",
+      [slot], // !!! define the slot
+      blockNumber,
+    ]);
+
+    const blockheader =
+      "0x1e34f1137efe68235a91b52a9afb6e30e08dcf86e25376a8867ebbebd463ca99";
+    // console.log(signature);
+    // console.log(proof);
+    const hashiVerifier = new ethers.Contract(
+      HASHI_VERIFIER_ADDRESS,
+      HASHI_VERIFIER_ABI,
+      hardhatSigner
+    );
+    // console.log(hashiVerifier);
+    console.log("blockNumber: ", blockNumber);
+    console.log("blockheader: ", blockheader);
+    console.log("blockHash: ", blockheader);
+    try {
+      const verifyOwnertx = await hashiVerifier.verifyOwner(
+        blockheader,
+        tokenId,
+        signature,
+        stateRoot,
+        proof.storageHash,
+        proof.accountProof,
+        proof.storageProof[0].proof
+      );
+
+      console.log(verifyOwnertx);
+      console.log("index: ", i);
+      console.log("slot: ", slot);
+    } catch (err) {
+      console.log("Error: ", err.message);
+    }
+  }
+}
+
+testContract(816);
